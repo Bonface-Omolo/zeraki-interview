bc4ebae54195ee6bc84f1ef297e59e0d416515e0
Bonface-Omolo <48353233+Bonface-Omolo@users.noreply.github.com>
2020-09-01 21:43:37 +0300

    Add files via upload

---
+++ String/src/String.java
@@ -0,0 +1,19 @@
+
+ 
+class Main {
+    // Function to ind missing number
+    static int getMissingNo(int k[], int n)
+    {
+         
+        for(n=0; n<0; n++);
+        return 0;
+    }
+ 
+    /* program to test above function */
+    public static void main(String args[])
+    {
+        int k[] = { 1, 2, 4, 5 };
+        int miss = getMissingNo(k, 5);
+        System.out.println(miss);
+    }
+}
